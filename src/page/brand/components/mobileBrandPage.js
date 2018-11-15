d4b9be999a1b258c8f388a880357ca726749beaa
AMin <alexanderocean16@gmail.com>
2018-11-15 12:24:48 +0800

    手機版形象頁面 建置

--- src/page/brand/components/mobileBrandPage.js
+++ src/page/brand/components/mobileBrandPage.js
@@ -1,29 +1,29 @@
 import React from 'react'
-import { Container, Responsive } from 'semantic-ui-react'
 
 import quapni_logo from '../../home/images/quapni_logo.svg'
 import cal_85th_gd_2 from '../../home/images/cal_85th_gd_2.jpg'
+import { Container,Responsive } from 'semantic-ui-react'
 
+import '../css/mobileBrandPage.css'
 
-import '../css/brandPage.css'
-
-export const Brand = () => (
-
+export const MobileBrand = () => (
+<Responsive maxWidth={900}>
   <div>
     <title>品牌故事 - Quapni</title>
     <Container>
       <img className="logo-style" alt="quapni_logo" src={quapni_logo} />
       <h1 className="slogan-style">突破 創新 舒適 精緻簡約</h1>
       <p className="Paragraph-style">
-        提供給愛好大自然的你們！<br />
-        有更好更健康的生活方式，<br />
-        產品研發 堅持『 舒適 』與『 創新』，<br />
+        提供所有愛好大自然有更好更健康的生活方式，<br />
+        而堅持研發舒適及創新觀感的產品。<br />
         突破現有的設計與生產技術，<br />
-        滿足操控性與使用性的最高境界。
+        以滿足操控性與使用性的最高境界。
+        Test
       </p>
     </Container>
     <div className="box">
-      <img src={cal_85th_gd_2}/>
+      <img src={cal_85th_gd_2} />
     </div>
   </div>
+</Responsive>
 )
